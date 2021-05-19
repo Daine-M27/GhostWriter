9b9d7daa44278d01654895c1a49fd611a9cbb4f6
Daine Marshall <dmarshall@thelightsource.com>
2021-05-19 16:51:54 -0400

    working typewriter and font

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SHALL WE PLAY A GAME?', subTitle: 'DMX TO RDM INTERFACE' });
 });
 
 module.exports = router;
